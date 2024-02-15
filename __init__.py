aa307d0a8906d63601ab17681f67267b43bfd0b5
Polluix <lipe311973@gmail.com>
2024-02-15 20:58:11 -0300

    removes unused imports

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
 import pygetwindow as gw
-import pyautogui
 from src.constants import propselector, width, height, topleft_x, topleft_y
 from functions.velocity import changeVelocity, setZero
+from functions.thrust import changeThrust
 
 
 propselector.activate()
 
 setZero()
 changeVelocity()
+changeThrust()
+
 
 
 # propselector.close()

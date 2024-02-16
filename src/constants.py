8d31236e12f7bdd6c2b278e384abbfc029d28707
Polluix <lipe311973@gmail.com>
2024-02-15 21:02:28 -0300

    add bottom right coordinates

--- src/constants.py
+++ src/constants.py
@@ -3,3 +3,4 @@ import pygetwindow as gw
 propselector = gw.getWindowsWithTitle(' Aerodesign Propeller Selector')[0]
 width, height = propselector.size
 topleft_x, topleft_y = propselector.topleft
+bottomright_x, bottomright_y = propselector.bottomright

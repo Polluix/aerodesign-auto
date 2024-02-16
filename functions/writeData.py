8f3275073530c0769bde2e7c0e851d96e009f346
Polluix <lipe311973@gmail.com>
2024-02-16 13:30:09 -0300

    adds function to write result data in the file

---
+++ functions/writeData.py
@@ -0,0 +1,21 @@
+def writeData(result, filename):
+    try:
+        file = open(f'./{filename}', 'a+')
+    except:
+        raise FileNotFoundError
+    
+    print(filename)
+
+    file.write('        {}            {}    {}           {}               {}                   {}                {}             {}           {}                  {}\n'.format(
+        result[0][1],
+        result[1][1],
+        result[4][1],
+        result[5][1],
+        result[6][1],
+        result[7][1],
+        result[8][1],
+        result[9][1],
+        result[10][1],
+        result[11][1])
+    )
+    file.close()

7887d9610948ef0965af0ba82cf8c0939696bf1d
Polluix <lipe311973@gmail.com>
2024-02-16 13:31:22 -0300

    adds data writing in the file created

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from functions.velocity import changeVelocity, setZero
 from functions.thrust import changeThrust
 from src.constants import *
 from functions.readImg import readImg
+from functions.writeData import writeData
 
 propselector.activate()
 
@@ -11,9 +12,19 @@ changeVelocity()
 changeThrust()
 
 result = readImg()
+filename = 'data.txt'
 
-n = len(result)
-for i in range(n):
-    print(result[i][1], i)
+try:
+    file = open(f'./{filename}', 'a+')
+except:
+    raise FileNotFoundError
+
+file.write('# Air Speed [m/s]         RPM      Thrust [N]      Power Output [W]    Power Absorbed [W]      Efficiency [%]      Prop Thrust Coeff   A/C Thrust Coeff    Tip Mach Number     075R Pitch Angle [deg]\n')
+file.close()
+
+# TODO: change air speed value and get data again 30 times.
+for i in range(30):
+    writeData(result, filename)
+
 
 # propselector.close()

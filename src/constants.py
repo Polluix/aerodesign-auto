71e092b7f579070af1b012081562b382233e9ca9
Polluix <lipe311973@gmail.com>
2024-02-16 11:13:07 -0300

    adds screenshot_path

--- src/constants.py
+++ src/constants.py
@@ -5,3 +5,4 @@ width, height = propselector.size
 topleft_x, topleft_y = propselector.topleft
 bottomright_x, bottomright_y = propselector.bottomright
 allow_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'E', '.', '+', '-', '_']
+screenshot_path = './result/result.png'

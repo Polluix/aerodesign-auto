5ee3142832c0697b6de7f18b25945548e5e74c4a
Polluix <lipe311973@gmail.com>
2024-02-15 21:16:40 -0300

    takes screenshot after initial manipulation

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
-import pygetwindow as gw
-from src.constants import propselector, width, height, topleft_x, topleft_y
+from src.constants import propselector
 from functions.velocity import changeVelocity, setZero
 from functions.thrust import changeThrust
-
+from src.constants import *
+import pyautogui
+from PIL import Image
 
 propselector.activate()
 
@@ -10,6 +11,13 @@ setZero()
 changeVelocity()
 changeThrust()
 
+#TODO: transform in function
+screenshot_path = './result/result.png'
+
+pyautogui.screenshot(screenshot_path)
+im = Image.open(screenshot_path)
+im = im.crop((topleft_x+10, topleft_y-10, bottomright_x-10, bottomright_y+10))
+im.save(screenshot_path)
 
 
 # propselector.close()

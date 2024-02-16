a0c859ef2d27b86fd0a11c2adf7f4131891bcc52
Polluix <lipe311973@gmail.com>
2024-02-16 11:08:20 -0300

    adds allow_list for reader

--- src/constants.py
+++ src/constants.py
@@ -4,3 +4,4 @@ propselector = gw.getWindowsWithTitle(' Aerodesign Propeller Selector')[0]
 width, height = propselector.size
 topleft_x, topleft_y = propselector.topleft
 bottomright_x, bottomright_y = propselector.bottomright
+allow_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'E', '.', '+', '-', '_']

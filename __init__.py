552e38822b044a7a6b57496952227e329bac6fa2
Polluix <lipe311973@gmail.com>
2024-02-16 11:25:14 -0300

    use readImg function and print the image text

--- __init__.py
+++ __init__.py
@@ -2,10 +2,7 @@ from src.constants import propselector
 from functions.velocity import changeVelocity, setZero
 from functions.thrust import changeThrust
 from src.constants import *
-import pyautogui
-from PIL import Image
-import pytesseract as pyt
-import cv2
+from functions.readImg import readImg
 
 propselector.activate()
 
@@ -13,20 +10,10 @@ setZero()
 changeVelocity()
 changeThrust()
 
-#TODO: transform in function
-screenshot_path = './result/result.png'
-pyt.pytesseract.tesseract_cmd = 'D:/Program Files/Tesseract-OCR/tesseract.exe'
+result = readImg()
 
-pyautogui.screenshot(screenshot_path)
-im = Image.open(screenshot_path)
-im = im.crop((topleft_x+10, topleft_y-10, bottomright_x-10, bottomright_y+10))
-im.save(screenshot_path)
-
-im = cv2.imread(screenshot_path)
-gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) 
-
-text = pyt.image_to_string(gray)
-print(text)
-
+n = len(result)
+for i in range(n):
+    print(result[i][1], i)
 
 # propselector.close()

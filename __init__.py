ec45df15ae734145dcc5231171e66991e1b4673e
Polluix <lipe311973@gmail.com>
2024-02-15 21:51:20 -0300

    takes screenshot and turn image into text

--- __init__.py
+++ __init__.py
@@ -4,6 +4,8 @@ from functions.thrust import changeThrust
 from src.constants import *
 import pyautogui
 from PIL import Image
+import pytesseract as pyt
+import cv2
 
 propselector.activate()
 
@@ -13,11 +15,18 @@ changeThrust()
 
 #TODO: transform in function
 screenshot_path = './result/result.png'
+pyt.pytesseract.tesseract_cmd = 'D:/Program Files/Tesseract-OCR/tesseract.exe'
 
 pyautogui.screenshot(screenshot_path)
 im = Image.open(screenshot_path)
 im = im.crop((topleft_x+10, topleft_y-10, bottomright_x-10, bottomright_y+10))
 im.save(screenshot_path)
+
+im = cv2.imread(screenshot_path)
+gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) 
+
+text = pyt.image_to_string(gray)
+print(text)
 
 
 # propselector.close()

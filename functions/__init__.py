0dcdc2891d3aa2e84551756d1f09631ace9f677e
Polluix <lipe311973@gmail.com>
2024-02-17 11:03:00 -0300

    changes function module location

--- functions/__init__.py
+++
@@ -1,4 +0,0 @@
-from functions.thrust import changeThrust
-from functions.velocity import setZero, changeVelocity, addOne
-from functions.readImg import readImg
-from functions.writeData import writeData
---
+++ src/functions/__init__.py
@@ -0,0 +1,4 @@
+from src.functions.thrust import changeThrust
+from src.functions.velocity import setZero, changeVelocity, addOne
+from src.functions.readImg import readImg
+from src.functions.writeData import writeData

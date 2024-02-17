7805be53c1972217b9a8ef8d8767661a3af12262
Polluix <lipe311973@gmail.com>
2024-02-17 15:03:36 -0300

    saves img on current working directory

--- src/functions/readImg.py
+++ src/functions/readImg.py
@@ -4,13 +4,20 @@ from PIL import Image
 import cv2
 import numpy as np
 import easyocr
+import os
+
 
 def readImg():
     pyautogui.screenshot(screenshot_path)
     img = Image.open(screenshot_path)
     img = img.crop((topleft_x+width*0.44, topleft_y+height*0.13, topleft_x+width*0.655, topleft_y+height*0.975))   
-    img.save(screenshot_path)
-    img = cv2.imread(screenshot_path)
+
+    current = os.getcwd()
+
+    img_name = f'{current}/image.png'
+
+    img.save(img_name)
+    img = cv2.imread(img_name)
 
     lab= cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
     l_channel, a, b = cv2.split(lab)

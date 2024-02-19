933b23142e69220265bd53b69631e32b0299595e
Polluix <lipe311973@gmail.com>
2024-02-19 14:52:30 -0300

    remove screenshot_path unused value

--- src/constants.py
+++ src/constants.py
@@ -1,8 +1,10 @@
 import pygetwindow as gw
+from src.getWindow import getWindow
 
-propselector = gw.getWindowsWithTitle(' Aerodesign Propeller Selector')[0]
+title = getWindow()
+
+propselector = gw.getWindowsWithTitle(title)[0]
 width, height = propselector.size
 topleft_x, topleft_y = propselector.topleft
 bottomright_x, bottomright_y = propselector.bottomright
 allow_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'E', '.', '+', '-', '_']
-screenshot_path = './result/result.png'
